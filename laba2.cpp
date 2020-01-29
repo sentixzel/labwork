d9ba13bca5f8b741cf8c5bc170d51141ed29c04d
sentixzel <59180152+sentixzel@users.noreply.github.com>
2020-01-29 15:57:56 +0300

    Update laba2.cpp
    Выделил размеры массивов в константу и в соответствующих местах менял значения на константу.

--- laba2.cpp
+++ laba2.cpp
@@ -1,42 +1,43 @@
 #include <iostream>
 
-int staticArray[4];
-int static2DArray[4][4];
+const int arrSize = 4;
+int staticArray[arrSize];
+int static2DArray[arrSize][arrSize];
 	
 int main()
 {
-	int *dynamicArray = new int[4];
+	int *dynamicArray = new int[arrSize];
 	
 	int **dynamic2DArray;
-	dynamic2DArray = new int*[4];
-	for (int i = 0; i < 4; ++i)
-		dynamic2DArray[i] = new int[4];
+	dynamic2DArray = new int*[arrSize];
+	for (int i = 0; i < arrSize; ++i)
+		dynamic2DArray[i] = new int[arrSize];
 	
 	std::cout << "Static 1-D: " << "\n" << " ";
-	for (int i = 0; i < 4; ++i)
+	for (int i = 0; i < arrSize; ++i)
 		std::cout << &staticArray[i] << " ";
 	
 	std::cout << "\n" << "\n";
 	
 	std::cout << "Static 2-D: " << "\n" << " ";
-	for (int i = 0; i < 4; ++i)
+	for (int i = 0; i < arrSize; ++i)
 	{
-		for (int j = 0; j < 4; ++j)
+		for (int j = 0; j < arrSize; ++j)
 			std::cout << &static2DArray[i][j] << " ";
 		std::cout << "\n" << " ";
 	}
 	std::cout << "\n";
 	
 	std::cout << "Dynamic 1-D: " << "\n" << " ";
-	for (int i = 0; i < 4; ++i)
+	for (int i = 0; i < arrSize; ++i)
 		std::cout << &dynamicArray[i] << " ";
 	
 	std::cout << "\n" << "\n";
 	
 	std::cout << "Dynamic 2-D: " << "\n" << " ";
-	for (int i = 0; i < 4; ++i)
+	for (int i = 0; i < arrSize; ++i)
 	{
-		for (int j = 0; j < 4; ++j)
+		for (int j = 0; j < arrSize; ++j)
 			std::cout << &dynamic2DArray[i][j] << " ";
 		std::cout << "\n" << " ";
 	}
